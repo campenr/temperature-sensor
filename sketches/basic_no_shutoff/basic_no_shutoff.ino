c1177cb5c059001318491f277f94bfba7fec0226
Richard Campen <richard@campen.co>
2020-12-29 09:57:12 +1300

    feat: initial commit

---
+++ sketches/basic_no_shutoff/basic_no_shutoff.ino
@@ -0,0 +1,24 @@
+/* Basic Sketch to read temperature from a TMP35/36/37 temperature sensor (no shutoff signal). */
+
+#define PIN_SENSOR A1
+#define SOURCE_VOLTAGE 5000  // in millivolts
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  printTemperature(readTemperature());
+  delay(1000);
+}
+
+float readTemperature() {
+  float voltage = analogRead(PIN_SENSOR) * (SOURCE_VOLTAGE / 1024.0);
+  return (voltage - 500) / 10;
+}
+
+void printTemperature(float value) {
+  Serial.print(value, 1);  // 
+  Serial.print(" \xC2\xB0");  // degree symbol
+  Serial.println("C");
+}
